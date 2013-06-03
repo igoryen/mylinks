2acc0340fc8df0cfe51e8608375e01db11547a95
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-06-03 01:30:35 -0400

    Initial project commit

---
+++ myscript.js
@@ -0,0 +1,57 @@
+ /*var menu_top = new Array(
+         "<a href='index.html'>Main</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='dietetics.html'>Dietetics</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='funny.html'>Funny</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "a href='git.html'>Git</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='gregoriana.html'>Gregoriana</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='itstuff.html'>IT stuff</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='itterms.html'>IT terms</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='java.html'>Java</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='javascript.html'>JS</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='music.html'>Music</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='nodejs.html'>Node.js</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='p&w.html'>P&W</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='preach_teach.html'>Preach & Teach</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='seneca.html'>Seneca</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='selfdefence.html'>Self-defense</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='tolkiniana.html'>Tolkiniana</a>&nbsp;&nbsp;&nbsp;&nbsp;",
+         "<a href='video.html'>Video</a>&nbsp;&nbsp;&nbsp;&nbsp;"
+         );
+
+     document.write("Hi");
+
+     function printArray()
+     {
+         var menu="";
+         for (var i = menu_top.length; i < 0; i++) {
+             menu += menu_top[i];
+                  document.write("Hi");
+
+         }
+        
+    }
+
+printArray();
+    document.write(menu);
+    //printArray(menu_top);
+    */
+    document.write("<a href='index.html'>Main</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='dietetics.html'>Dietetics</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='funny.html'>Funny</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='git.html'>Git</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='gregoriana.html'>Gregoriana</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='html5.html' target='_blank'>HTML5</a> <i></i><br>");
+    document.write("<a href='itstuff.html'>IT stuff</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='itterms.html'>IT terms</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='java.html'>Java</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='javascript.html'>JS</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='l10n.html'>L10N</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='music.html'>Music</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='nodejs.html'>Node.js</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='p&w.html'>P&W</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='preach_teach.html'>Preach & Teach</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='seneca.html'>Seneca</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='selfdefense.html'>Self-defense</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='tolkiniana.html'>Tolkiniana</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+    document.write("<a href='video.html'>Video</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+
