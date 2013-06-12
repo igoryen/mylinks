10c7b1408841c94172d3fc250bf5d2f2ed13ad9a
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-06-11 21:41:28 -0400

    130611t 21:41 - put a loop into myscript.js

--- myscript.js
+++ myscript.js
@@ -35,23 +35,43 @@ printArray();
     document.write(menu);
     //printArray(menu_top);
     */
-    document.write("<a href='index.html'>Main</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='dietetics.html'>Dietetics</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='funny.html'>Funny</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='git.html'>Git</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='gregoriana.html'>Gregoriana</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='html5.html' target='_blank'>HTML5</a> <i></i><br>");
-    document.write("<a href='itstuff.html'>IT stuff</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='itterms.html'>IT terms</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='java.html'>Java</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='javascript.html'>JS</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='l10n.html'>L10N</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='music.html'>Music</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='nodejs.html'>Node.js</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='p&w.html'>P&W</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='preach_teach.html'>Preach & Teach</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='seneca.html'>Seneca</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='selfdefense.html'>Self-defense</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='tolkiniana.html'>Tolkiniana</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
-    document.write("<a href='video.html'>Video</a>&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>");
+
+    var pinks = [
+    'a', "b", "c"
+
+    ]
+
+    var links = [ 
+    "<a href='index.html'>Main</a>"
+    ,"<a href='dietetics.html'>Dietetics</a>"
+    ,"<a href='funny.html'>Funny</a>"
+    ,"<a href='git.html'>Git</a>"
+    ,"<a href='gregoriana.html'>Gregoriana</a>"
+    ,"<a href='html5.html'>HTML5</a>"
+    ,"<a href='itstuff.html'>IT stuff</a>"
+    ,"<a href='itterms.html'>IT terms</a>"
+    ,"<a href='java.html'>Java</a>"
+    ,"<a href='javascript.html'>JS</a>"
+    ,"<a href='l10n.html'>L10N</a>"
+    ,"<a href='music.html'>Music</a>"
+    ,"<a href='nodejs.html'>Node.js</a>"
+    ,"<a href='p&w.html'>P&W</a>"
+    ,"<a href='preach_teach.html'>Preach & Teach</a>"
+    ,"<a href='seneca.html'>Seneca</a>"
+    ,"<a href='selfdefense.html'>Self-defense</a>"
+    ,"<a href='tolkiniana.html'>Tolkiniana</a>"
+    ,"<a href='video.html'>Video</a>"
+    ];
+
+    var arr = links;
+
+if (arr == links){
+
+    for (var i in arr) {
+        document.write(arr[i] + "<br>"); 
+    };
+} else{
+    document.write("blah");
+}
+
 
