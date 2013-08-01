a37905d95e7877fa1368866423842feaed75282b
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-08-01 12:03:18 -0400

    removed ver1html

--- ver1htmlfiles/bbb.js
+++
@@ -1,8 +0,0 @@
-
-var git_ = [
-'<a href="https://github.com/fletcher/MultiMarkdown/blob/master/Documentation/Markdown%20Syntax.md" target="_blank">Markdown Syntax</a> <i></i><br>'
-,'<a href="https://github.com/mozilla/popcorn_maker" target="_blank">popcorn maker</a> <i></i><br>'
-,'<a href="https://github.com/mozilla/node-webmaker-loginapi" target="_blank">node-webmaker-loginapi</a> <i></i><br>'
-,'<a href="https://github.com/mozilla/thimble.webmaker.org" target="_blank">thimble.webmaker.org</a> <i></i><br>'
-,'<a href="https://github.com/mozilla/webmaker.org" target="_blank">webmaker.org</a> <i></i><br>'
-];
